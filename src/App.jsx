cd67f0ef87df0d4b39af81017e862fd3eb1f6da8
Vaibhav Bergal <bergalvaibhav33@gmail.com>
2024-02-11 23:27:08 +0530

    OTP Login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex justify-center min-h-screen bg-slate-500">
-        <div className="flex flex-col items-center w-full max-w-md gap-8 pt-5 mt-24 shadow-lg h-52 bg-zinc-300 rounded-2xl">
+        <div className="flex flex-col items-center w-full max-w-md gap-8 pt-5 mt-24 shadow-lg h-52 bg-zinc-300 rounded-2xl ">
           <h1 className="text-2xl font-medium ">Login with Phone</h1>
           <OtpLoginForm />
         </div>
